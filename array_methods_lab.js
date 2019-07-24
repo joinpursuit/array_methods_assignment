594957934639b539cc0d8bce83330c2ca7f6431b
Mike <michaelamparo@pursuit.org>
2019-07-23 23:17:58 -0400

    rewrote as function

--- array_methods_lab.js
+++ array_methods_lab.js
@@ -13,8 +13,9 @@ Output: [11,12,13,14,15]
 \n----------------------------------------\n\n`);
 
 let qar1 = [1, 2, 3, 4, 5]
-let ans1 = qar1.map(i => i + 10)
-console.log(ans1);
+const plus10toAll = (arr) => arr.map(i => i + 10)
+// let ans1 = qar1.map(i => i + 10)
+console.log(plus10toAll(qar1));
 
 //## Question 2.
 console.log(`\n\n\n\n========================================\n## Question 2.\n===============`);
@@ -29,8 +30,9 @@ Output: ["a", "", "b", "c", ""]
 \n----------------------------------------\n\n`);
 
 let qar2 = ["a", 123, "b", "c", {name: "cat"}]
-let ans2 = qar2.map(i => typeof i === `string` ? i : i = ``)
-console.log(ans2);
+const stringsOnly = (arr) => arr.map(i => typeof i === `string` ? i : i = ``)
+// let ans2 = qar2.map(i => typeof i === `string` ? i : i = ``)
+console.log(stringsOnly(qar2));
 
 //## Question 3.
 console.log(`\n\n\n\n========================================\n## Question 3.\n===============`);
@@ -45,8 +47,9 @@ Output: [2,4]
 \n----------------------------------------\n\n`);
 
 let qar3 = [1, 2, 3, 4, 5]
-let ans3 = qar3.filter(i => !(i % 2))
-console.log(ans3);
+const onlyEven = (arr) => arr.filter(i => !(i % 2))
+// let ans3 = qar3.filter(i => !(i % 2))
+console.log(onlyEven(qar3));
 
 //## Question 4.
 console.log(`\n\n\n\n========================================\n## Question 4.\n===============`);
@@ -64,12 +67,13 @@ Output: false
 \n----------------------------------------\n\n`);
 
 let qar4 = [1, 1, 1, 1, 1]
-let ans4 = qar4.every(i => i === qar4[0])
-console.log(ans4);
+const allSame = (arr) => arr.every(i => i === arr[0])
+// let ans4 = qar4.every(i => i === qar4[0])
+console.log(allSame(qar4));
 
 let qar4_2 = [1, 1, 1, 1, 2]
-let ans4_2 = qar4_2.every(i => i === qar4_2[0])
-console.log(ans4_2);
+// let ans4_2 = qar4_2.every(i => i === qar4_2[0])
+console.log(allSame(qar4_2));
 
 //## Question 5.
 console.log(`\n\n\n\n========================================\n## Question 5.\n===============`);
@@ -84,8 +88,9 @@ Output: 15
 \n----------------------------------------\n\n`);
 
 let qar5 = [1, 2, 3, 4, 5]
-let ans5 = qar5.reduce((i, j) => i + j)
-console.log(ans5);
+const sumOfAllElem = (arr) => arr.reduce((i, j) => i + j)
+// let ans5 = qar5.reduce((i, j) => i + j)
+console.log(sumOfAllElem(qar5));
 
 //## Question 6.
 console.log(`\n\n\n\n========================================\n## Question 6.\n===============`);
@@ -100,8 +105,9 @@ Output: [6]
 \n----------------------------------------\n\n`);
 
 let qar6 = [1, 2, 6, 3, 4, 5]
-let ans6 = qar6.reduce((i, j) => i > j ? i : j)
-console.log(ans6);
+const largestInArray = (arr) => arr.reduce((i, j) => i > j ? i : j)
+// let ans6 = qar6.reduce((i, j) => i > j ? i : j)
+console.log(largestInArray(qar6));
 
 //## Question 7.
 console.log(`\n\n\n\n========================================\n## Question 7.\n===============`);
@@ -116,8 +122,9 @@ Output: 6
 \n----------------------------------------\n\n`);
 
 let qar7 = [1, 0, 0, 2, 0, 3, 0, 0, 0]
-let ans7 = qar7.reduce((i, j) => j === 0 ? i += 1 : i, 0)
-console.log(ans7);
+const howMany0s = (arr) => arr.reduce((i, j) => j === 0 ? i += 1 : i, 0)
+// let ans7 = qar7.reduce((i, j) => j === 0 ? i += 1 : i, 0)
+console.log(howMany0s(qar7));
 
 //## Question 8.
 console.log(`\n\n\n\n========================================\n## Question 8.\n===============`);
@@ -132,8 +139,9 @@ Output: [2,4,6,8]
 \n----------------------------------------\n\n`);
 
 let qar8 = [1, 2, 3, 4]
-let ans8 = qar8.map(i => i * 2)
-console.log(ans8);
+const doubleAllElem = (arr) => arr.map(i => i * 2)
+// let ans8 = qar8.map(i => i * 2)
+console.log(doubleAllElem(qar8));
 
 //## Question 9.
 console.log(`\n\n\n\n========================================\n## Question 9.\n===============`);
@@ -148,8 +156,9 @@ Output: 384
 \n----------------------------------------\n\n`);
 
 let qar9 = [1, 2, 3, 4]
-let ans9 = qar9.reduce((i, j) => i * j * 2, 1)
-console.log(ans9);
+const productOfAllDoubled = (arr) => arr.reduce((i, j) => i * j * 2, 1)
+// let ans9 = qar9.reduce((i, j) => i * j * 2, 1)
+console.log(productOfAllDoubled(qar9));
 
 //## Question 10.
 console.log(`\n\n\n\n========================================\n## Question 10.\n===============`);
@@ -163,10 +172,10 @@ Output: [5,10,15]
 
 \n----------------------------------------\n\n`);
 
-const elementDivisibleBy = (divisor, arr) => arr.filter(i => !(i % divisor))
 let qar10 = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-let ans10 = elementDivisibleBy(5, qar10)
-console.log(ans10);
+const elementDivisibleBy = (divisor, arr) => arr.filter(i => !(i % divisor))
+// let ans10 = qar10.filter(i => !(i % 5))
+console.log(elementDivisibleBy(5, qar10));
 
 //## Question 11.
 console.log(`\n\n\n\n========================================\n## Question 11.\n===============`);
@@ -181,8 +190,9 @@ Output: [0,7,16,27]
 \n----------------------------------------\n\n`);
 
 let qar11 = [6, 7, 8, 9]
-let numberTimesIdx = qar11.map((elm, i) => elm * i)
-console.log(numberTimesIdx);
+const numberTimesIdx = (arr) => arr.map((elm, i) => elm * i)
+// let numberTimesIdx = qar11.map((elm, i) => elm * i)
+console.log(numberTimesIdx(qar11));
 
 //## Question 12.
 console.log(`\n\n\n\n========================================\n## Question 12.\n===============`);
@@ -200,12 +210,13 @@ Output: false
 \n----------------------------------------\n\n`);
 
 let qar12 = [2, 4, 6, 8, 12]
-let ans12 = qar12.every(i => i > 0 && i % 10)
-console.log(ans12);
+const no10sOrNegatives = (arr) => arr.every(i => i > 0 && i % 10)
+// let ans12 = qar12.every(i => i > 0 && i % 10)
+console.log(no10sOrNegatives(qar12));
 
 let qar12_2 = [2, 4, 6, 8, 10]
-let ans12_2 = qar12_2.every(i => i > 0 && i % 10)
-console.log(ans12_2);
+// let ans12_2 = qar12_2.every(i => i > 0 && i % 10)
+console.log(no10sOrNegatives(qar12_2));
 
 //## Question 13.
 console.log(`\n\n\n\n========================================\n## Question 13.\n===============`);
@@ -220,8 +231,9 @@ Output: 9
 \n----------------------------------------\n\n`);
 
 let qar13 = [1, 2, 3, 4, 5]
-let ans13 = qar13.reduce((i, j) => j % 2 ? i += j : i)
-console.log(ans13);
+const sumOfAllOdd = (arr) => arr.reduce((i, j) => j % 2 ? i += j : i)
+// let ans13 = qar13.reduce((i, j) => j % 2 ? i += j : i)
+console.log(sumOfAllOdd(qar13));
 
 //## Question 14.
 console.log(`\n\n\n\n========================================\n## Question 14.\n===============`);
@@ -236,8 +248,9 @@ Output: ["CAT", "DOG", "BEAR"]
 \n----------------------------------------\n\n`);
 
 let qar14 = [{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]
-let ans14 = qar14.filter(i => typeof i === `string`).map(i => i.toUpperCase())
-console.log(ans14);
+const stringsOnlyUpperCase = (arr) => arr.filter(i => typeof i === `string`).map(i => i.toUpperCase())
+// let ans14 = qar14.filter(i => typeof i === `string`).map(i => i.toUpperCase())
+console.log(stringsOnlyUpperCase(qar14));
 
 //## Bonus:
 console.log(`\n\n\n\n========================================\n## Bonus:\n===============`);
