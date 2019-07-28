6626bf0c36a9b0bebdd624528b16c87b3921076c
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-07-28 02:05:40 -0400

    v.5, completed, + minor revisions for clarity

--- array_methods_lab.js
+++ array_methods_lab.js
@@ -4,11 +4,11 @@
 
 
 // global function declarations
-const print = function(vari, bool) {
+const print = function(forDisplay, bool) {
   if (bool) { // for outputs
-    console.log(vari,'\n');
+    console.log(forDisplay,'\n');
   } else { // for inputs
-    console.log(`Input: [${vari}]`);
+    console.log(`Input: [${forDisplay}]`);
   }
 }
 
@@ -16,151 +16,149 @@ const print = function(vari, bool) {
 
 // 1 //
 console.log("1 // Given an array of numbers, write a function that returns a new array where every element is incremented by 10.");
-let input_01 = [1,2,3,4,5];
-print(input_01);
+let input_1 = [1,2,3,4,5];
+print(input_1);
 
-let result01 = input_01.map( function(num) {
-    return num + 10;
+let result_1 = input_1.map( function(el) {
+    return el + 10;
 } );
-print(result01, 1);
+print(result_1, 1);
 //
 
 
 // 2 //
 console.log("2 // Given an array, return a new array where every non-string element is converted to an empty string.");
-let input_02 = ["a", 123, "b", "c", {name: "cat"}];
+let input_2 = ["a", 123, "b", "c", {name: "cat"}];
 print('["a", 123, "b", "c", {name: "cat"}]');
 
-let result02 = [];
-input_02.forEach( function(el) {
+let result_2 = [];
+input_2.forEach( function(el) {
     if (typeof el === "string") {
-      result02.push(el);
+      result_2.push(el);
     } else {
-      result02.push('');
+      result_2.push('');
     }
 } );
-print(result02, 1);
+print(result_2, 1);
 //
 
 
 // 3 //
 console.log("3 // Write a function that returns only the even elements in an array of numbers.");
-let input_03 = [1,2,3,4,5];
-print(input_03);
+let input_3 = [1,2,3,4,5];
+print(input_3);
 
-const onlyTheEvens = (arr) => {
+const toOnlyEvens = (input_Arr) => {
   return (
-    arr.filter( (el) => !(el % 2) )
+    input_Arr.filter( (el) => !(el % 2) )
   );
 }
-let result03 = onlyTheEvens(input_03);
-print(result03, 1);
+let result_3 = toOnlyEvens(input_3);
+print(result_3, 1);
 //
 
 
 // 4 //
 console.log("4 // Write a function that checks if all elements in the array are the same.");
-let input_04 = [1,1,1,1,"1"]; // [1,1,1,1,2]
+let input_4 = [1,1,1,1,"1"]; // [1,1,1,1,2]
 print('[1,1,1,1,"1"]');
 
-const allTheSame = (arr) => {
+const areAllSame = (input_Arr) => {
   return (
-    arr.every( el => el === arr[0] )
+    input_Arr.every( el => el === input_Arr[0] )
   );
 }
-let result04 = allTheSame(input_04);
-print(result04, 1);
+let result_4 = areAllSame(input_4);
+print(result_4, 1);
 //
 
 
 // 5 //
 console.log("5 // Write a function that returns the sum of every element in an array.");
-let input_05 = [1,2,3,4,5];
-print(input_05);
+let input_5 = [1,2,3,4,5];
+print(input_5);
 
-const arrSum = function(arr) {
+const sumArr = function(input_Arr) {
   return (
-    arr.reduce( function(acc, curr) {
+    input_Arr.reduce( function(acc, curr) {
         return acc + curr;
     }, 0)
   );
 }
-let result05 = arrSum(input_05);
-print(result05, 1);
+let result_5 = sumArr(input_5);
+print(result_5, 1);
 //
 
 
 // 6 //
 console.log("6 // Write a function that finds largest number in an array.");
-let input_06 = [1,2,6,3,4,8,5];
-print(input_06);
+let input_6 = [1,2,6,3,4,8,5];
+print(input_6);
 
-const largestOfArr = function(arr) {
+const toFindLargest = function(input_Arr) {
   return (
-    arr.reduce( function(acc, curr) {
-        // console.log(acc); // debug
+    input_Arr.reduce( function(acc, curr) {
         if (curr > acc) {
-          return curr;
+          return curr
         }
-        return acc;
+        return acc
     } )
   );
 }
-let result06 = largestOfArr(input_06);
-print(result06, 1);
+let result_6 = toFindLargest(input_6);
+print(result_6, 1);
 //
 
 
 // 7 //
 console.log("7 // Write a function `countZeroes`, which takes an array of numbers as its argument and returns the amount of zeroes that occur in it.");
-let input_07 = [1,0,0,2,0,3,0,0,0];
-print(input_07);
+let input_7 = [1,0,0,2,0,3,0,0,0];
+print(input_7);
 
-const zeroCounter = (arr) => {
+const countZeroes = (input_Arr) => {
   return (
-    arr.reduce( (acc, curr) => {
-        //console.log(acc); // debug
+    input_Arr.reduce( (acc, curr) => {
         if (!(curr)) {
-          acc += 1;
+          return acc += 1;
         }
         return acc;
     }, 0)
   );
 }
-let result07 = zeroCounter(input_07);
-print(result07, 1);
+let result_7 = countZeroes(input_7);
+print(result_7, 1);
 //
 
 
 // 8 //
 console.log("8 // Write a function that doubles every element in an array.");
-let input_08 = [1,2,3,4];
-print(input_08);
+let input_8 = [1,2,3,4];
+print(input_8);
 
-const doubleTheEls = (arr) => {
+const doubleAll = (input_Arr) => {
   return (
-    arr.map(el => el * 2)
+    input_Arr.map(el => el * 2)
   );
 }
-let result08 = doubleTheEls(input_08);
-print(result08, 1);
+let result_8 = doubleAll(input_8);
+print(result_8, 1);
 //
 
 
 // 9 //
 console.log("9. Write a function that finds the sum of every element in an array doubled");
-let input_09 = [1,2,3,4];
-print(input_09);
+let input_9 = [1,2,3,4];
+print(input_9);
 
-const doubleArrSum = function(arr) {
+const doubleSumArr = function(input_Arr) {
   return (
-    2 * arr.reduce( function(acc, curr) {
+    2 * input_Arr.reduce( function(acc, curr) {
         return acc += curr;
     }, 0)
   )
 }
-let result09 = doubleArrSum(input_09);
-print(result09, 1);
+let result_9 = doubleSumArr(input_9);
+print(result_9, 1);
 //
 
 
@@ -187,9 +185,9 @@ console.log("11 // Write a `numberTimesIdx` that uses `map` and multiples each n
 let input_11 = [6,7,8,9];
 print(input_11);
 
-const numberTimesIdx = (arr) => {
+const numberTimesIdx = (input_Arr) => {
   return (
-    arr.map( (el, i) => el * i )
+    input_Arr.map( (el, i) => el * i )
   )
 }
 let result11 = numberTimesIdx(input_11);
@@ -203,12 +201,12 @@ let input_12 = [2,4,6,8,12]; // [2,4,6,8,10]
 print(input_12);
 
 // input_12.forEach( el => console.log( el > 0, !(el % 2), el % 10 )); // debug
-const checkAllEvenPosNoZeros = (arr) => {
+const areAllEvenPosNoZeroEnd = (input_Arr) => {
   return (
-    arr.every( el => el > 0 && !(el % 2) && el % 10 )
+    input_Arr.every( el => el > 0 && !(el % 2) && el % 10 )
   )
 }
-let result12 = checkAllEvenPosNoZeros(input_12);
+let result12 = areAllEvenPosNoZeroEnd(input_12);
 print(result12, 1);
 //
 
@@ -218,10 +216,9 @@ console.log("13 // Write a function that returns the sum of all odd numbers in a
 let input_13 = [2,3,4,346,-10.2,10001,5]; // [1,2,3,4,5]
 print(input_13);
 
-const sumTheOdds = function(arr) {
+const sumTheOdds = function(input_Arr) {
   return (
-    arr.reduce( function(acc, curr) {
-        // console.log(curr) // debug
+    input_Arr.reduce( function(acc, curr) {
         if (curr % 2 && !(curr % 1)) {
           return acc += curr
         }
@@ -239,15 +236,15 @@ console.log("14 // Write a function that returns a new array containing all of t
 let input_14 = [{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"];
 console.log('[{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]');
 
-const onlyToCap = function(arr) {
+const onlyStrsCapped = function(input_Arr) {
   return (
-    arr.filter( function(el) {
+    input_Arr.filter( function(el) {
         return (typeof el === "string")
     })
-    .map( el => el.toUpperCase() )
+    .map( str => str.toUpperCase() )
   )
 }
-let result14 = onlyToCap(input_14);
+let result14 = onlyStrsCapped(input_14);
 print(result14, 1);
 //
 
@@ -257,238 +254,219 @@ console.log('\n\n=== BONUS QUESTIONS ===\n');
 
 
 // b1 //
-console.log("1. Using Array.prototype add a new method `myForEach` that mimics the behavior of the built in `forEach`.");
-let input_b01 = [2,3,4,6,7,8];
-console.log(`Input: [${input_b01}]\nOutputting console.log with no return`);
+console.log("b1 // Using Array.prototype add a new method `myForEach` that mimics the behavior of the built in `forEach`.");
+let input_b1 = [2,3,4,6,7,8];
+console.log(`Input: [${input_b1}]\nOutputting console.log with no return`);
 
 Object.defineProperty(Array.prototype, 'myForEach', {
-    value: function(callback) {
+    value: function(toSelectEach) {
       for (let i of this) {
-        callback(i);
+        toSelectEach(i);
       }
     }
 } );
-let resultb01 =
-  input_b01.myForEach( function(el) {
+let result_b1 =
+  input_b1.myForEach( function(el) {
       console.log(el)
   } );
-print(resultb01, 1);
+print(result_b1, 1);
 //
 
 
 // b2 //
-console.log("2. Use your `myForEach` to add `myMap` to the Array prototype. `myMap` should behave the same as regular `map`.");
-console.log(`Input: [${input_b01}]\nOutputting mapping to (el * 5)`);
+console.log("b2 // Use your `myForEach` to add `myMap` to the Array prototype. `myMap` should behave the same as regular `map`.");
+console.log(`Input: [${input_b1}]\nOutputting mapping to (el * 5)`);
 
 Object.defineProperty(Array.prototype, 'myMap', {
-    value: function(callback) {
-      let outputArray = [];
-      this.myForEach( function(myForEachThing) {
-          outputArray.push(callback(myForEachThing));
+    value: function(toApplyMap) {
+      let outputMappedArray = [];
+      this.myForEach( function(toSelectEach) {
+          outputMappedArray.push(toApplyMap(toSelectEach));
       } );
-      return outputArray;
+      return outputMappedArray;
     }
 } );
-let resultb02 =
-  input_b01.myMap( function(el) {
+let result_b2 =
+  input_b1.myMap( function(el) {
       return el * 5;
   } );
-print(resultb02, 1);
+print(result_b2, 1);
 //
 
 
 // b3 //
-console.log("3. Write a `myFilter` that behaves the same as `filter`.");
-console.log(`Input: [${input_b01}]\nOutputting filtering to only (el > 6)`);
+console.log("b3 // Write a `myFilter` that behaves the same as `filter`.");
+console.log(`Input: [${input_b1}]\nOutputting filtering to only (el > 6)`);
 
 Object.defineProperty(Array.prototype, 'myFilter', {
-    value: function(callback) {
-      let outputArray = [];
-      this.myForEach( (myForEachThing) => {
-          if (callback(myForEachThing)) {
-              outputArray.push(myForEachThing);
+    value: function(isWhat) {
+      let outputFilteredArray = [];
+      this.myForEach( (toSelectEach) => {
+          if (isWhat(toSelectEach)) {
+              outputFilteredArray.push(toSelectEach);
           }
       } );
-      return outputArray;
+      return outputFilteredArray;
     }
 } );
-let resultb03 = input_b01.myFilter( (el) => (el > 6) );
-print(resultb03, 1);
+let result_b3 = input_b1.myFilter( (el) => (el > 6) );
+print(result_b3, 1);
 //
 
 
 // b4 //
-console.log("4. Write a `myEvery` that behaves the same as `every`.");
-let input_b04 = [1,2,'downbeat',4] // 5,6,7,8]
+console.log("b4 // Write a `myEvery` that behaves the same as `every`.");
+let input_b4 = [1,2,'downbeat',4] // 5,6,7,8]
 console.log(`Input: [1,2,'downbeat',4]\nOutputting checking if all elements are numbers`);
 
 Object.defineProperty(Array.prototype, 'myEvery', {
-    value: function(callback) {
-      for (let i of this) {
-        if (callback(i) === false) {
+    value: function(areAllWhat) {
+      for (let el of this) {
+        if (areAllWhat(el) === false) {
           return false;
         }
       }
       return true;
     }
 } );
-let resultb04 = input_b04.myEvery( (el => (typeof el === 'number')) );
-print(resultb04, 1);
+let result_b4 = input_b4.myEvery( (el => (typeof el === 'number')) );
+print(result_b4, 1);
 //
 
 
 // b5 //
-console.log("5. Write a `myReduce`. It should use the first element in the array if none is given.");
-let input_b05 = [4,8,12,16];
-console.log(`Input: [${input_b05}]\nOutputting sum of all elements`);
+console.log("b5 // Write a `myReduce`. It should use the first element in the array if none is given.");
+let input_b5 = [4,8,12,16];
+console.log(`Input: [${input_b5}]\nOutputting sum of all elements`);
 
 Object.defineProperty(Array.prototype, 'myReduce', {
-    value: function(callback, initValue) {
-      let pmAcc;
+    value: function(toReduceHow, initValue) {
+      let acc;
       if (initValue === undefined) {
-        pmAcc = this[0];
+        acc = this[0];
         for (let i = 1; i < this.length; i++) {
-          pmAcc = callback(pmAcc, this[i]);
+          acc = toReduceHow(acc, this[i]);
         }
       } else {
-        pmAcc = initValue;
+        acc = initValue;
         for (let i = 0; i < this.length; i++) {
-          pmAcc = callback(pmAcc, this[i]);
+          acc = toReduceHow(acc, this[i]);
         }
       }
-      return pmAcc;
+      return acc;
     }
 } );
-let resultb05 =
-  input_b05.myReduce( function(acc, curr) {
-      return acc += curr;
+let result_b5 =
+  input_b5.myReduce( function(sum, currentEl) {
+      return sum += currentEl;
 } );
-print(resultb05, 1);
+print(result_b5, 1);
 //
 
 
 // b6 //
-console.log("6. Write a `myJoin` function.");
-let input_b06 = ['ichi', 'ni', 'san', 'shi'];
-console.log(`Input: ${input_b06}. Join with '-'`);
+console.log("b6 // Write a `myJoin` function.");
+let input_b6 = ['ichi', 'ni', 'san', 'shi'];
+console.log(`Input: ${input_b6}. Join with '-'`);
 
 Object.defineProperty(Array.prototype, 'myJoin', {
-    value: function(pmSeparator) {
-      let newStr = this[0];
-      pmSeparator === undefined
-        ? pmSeparator = ','
+    value: function(separator) {
+      let outputStr = this[0];
+      separator === undefined
+        ? separator = ','
         : false;
       for (let i = 1; i < this.length; i++) {
-        newStr += (pmSeparator + this[i]);
+        outputStr += (separator + this[i]);
       }
-      return newStr;
+      return outputStr;
     }
 } );
-let resultb06 = input_b06.myJoin('-');
-print(resultb06, 1);
+let result_b6 = input_b6.myJoin('-');
+print(result_b6, 1);
 //
 
 
 // b7 //
-console.log("7. Write a `mySlice` function.");
-let input_b07 = ['a','b','l','i','o','n','c'];
+console.log("b7 // Write a `mySlice` function.");
+let input_b7 = ['a','b','l','i','o','n','c'];
 console.log(`Input: ['a','b','l','i','o','n','c']`);
 
 Object.defineProperty(Array.prototype, 'mySlice', {
-    value: function(pmBegin, pmEnd) {
+    value: function(startIdx, endIdx) {
       let outputArray = [];
 
-      // formalize pmBegin
-      if (pmBegin === undefined) {
-        pmBegin = 0;
+      // // formalize startIdx
+      if (startIdx === undefined) {
+        startIdx = 0;
       }
-      if (pmBegin > this.length) {
+      if (startIdx > this.length) {
         return outputArray;
       }
-      if (pmBegin < 0) {
-        pmBegin = this.length + pmBegin;
+      if (startIdx < 0) {
+        startIdx = this.length + startIdx;
       }
 
-      // formalize pmEnd
-      if (pmEnd === undefined || pmEnd > this.length) {
-        pmEnd = this.length;
+      // // formalize endIdx
+      if (endIdx === undefined || endIdx > this.length) {
+        endIdx = this.length;
       }
-      if (pmEnd < 0) {
-        pmEnd = this.length + pmEnd;
+      if (endIdx < 0) {
+        endIdx = this.length + endIdx;
       }
 
-      for (let i = pmBegin; i < pmEnd; i++) {
+      for (let i = startIdx; i < endIdx; i++) {
         outputArray.push(this[i]);
       }
       return outputArray;
     }
 } );
-let resultb07 = input_b07.mySlice(-5,-1);
-print(resultb07, 1);
+let result_b7 = input_b7.mySlice(-5,-1);
+print(result_b7, 1);
 //
 
 
 // b8 //
-console.log("8. Write a `myTranspose`. This function should transpose a matrix.");
-let input_b08 = [
+console.log("b8 // Write a `myTranspose`. This function should transpose a matrix.");
+let input_b8 = [
   [1, 2], [3, 4], [5, 6]
 ];
 console.log('[ [1, 2], [3, 4], [5, 6] ]');
 
 Object.defineProperty(Array.prototype, 'myTranspose', {
     value: function() {
-      let subOutput_Lefts = [];
-      let subOutput_Rights = [];
-      let outputArray = [subOutput_Lefts, subOutput_Rights];
+      let xOutput_Lefts = [];
+      let xOutput_Rights = [];
+      let outputArray = [xOutput_Lefts, xOutput_Rights];
       for (let i of this) {
-        subOutput_Lefts.push(i[0]);
-        subOutput_Rights.push(i[1]);
+        xOutput_Lefts.push(i[0]);
+        xOutput_Rights.push(i[1]);
       }
       return outputArray;
     }
 } );
-let resultb08 = input_b08.myTranspose();
-print(resultb08, 1);
+let result_b8 = input_b8.myTranspose();
+print(result_b8, 1);
 //
 
-/* UNFINISHED ======
+
 // b9 //
-console.log("9. Write a `myFlatten`. This should take a multi-dimensional array and return it as one array.");
-let input_b09 = [[[-3, -2], -1, 0], 1, 2, [3, 4, 5, [6, 7, 8]]];
+console.log("b9 // Write a `myFlatten`. This should take a multi-dimensional array and return it as one array.");
+let input_b9 = [[[-3, -2], -1, 0], 1, 2, [3, 4, 5, [6, 7, 8]]];
 console.log('[[[-3, -2], -1, 0], 1, 2, [3, 4, 5, [6, 7, 8]]]');
 
 Object.defineProperty(Array.prototype, 'myFlatten', {
     value: function() {
-      let isStillArray;
-      while (isStillArray !== false) {
-        this.reduce( (acc, curr) => {
-                  if (Array.isArray(curr)) {
-                    isStillArray = true
-                  } else {
-                    isStillArray = false
-                  }
-
-                  acc.concat(curr);
-              }, [])
+      let outputArray = [];
+      for (let i = 0; i < this.length; i++) {
+        if (Array.isArray(this[i])) {
+          outputArray = outputArray.concat(this[i].myFlatten());
+        } else {
+          outputArray.push(this[i]);
+        }
       }
-    //   let areArrays;
-    //   return (
-    //
-    //     while (areArrays !== false) {
-    //       this.reduce( (acc, curr) => {
-    //           if (Array.isArray(curr)) {
-    //             areArrays = true
-    //           } else {
-    //             areArrays = false
-    //           }
-    //           return acc.concat(curr)
-    //       }, [])
-    //     }
-    //   );
-    // }
+      return outputArray;
     }
 } );
-let resultb09 = input_b09.myFlatten();
-print(resultb09, 1);
+let result_b9 = input_b9.myFlatten();
+print(result_b9, 1);
 //
-*/
