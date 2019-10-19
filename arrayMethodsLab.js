61419c856585062c789839c29bb7a98f40cdc3f6
Brandon Brown <brandonb0206@gmail.com>
2019-10-19 15:00:34 -0400

    QUESTION 2 COMPLETED

--- arrayMethodsLab.js
+++ arrayMethodsLab.js
@@ -6,3 +6,16 @@ const add1 = (arr) => {
     })
 }
 console.log(add1([1, 2, 3, 4]))
+
+//QUESTION 2
+
+const nonString = (arr) => {
+    return arr.map((el) => {
+        if(typeof el === typeof ""){
+            return el
+        }else {
+            return ""
+        }
+    })
+}
+console.log(nonString([1, 2, "talk", 4, "to"]))
