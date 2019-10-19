52d5f935e718cf0b07a791cfba340c699da4e6b7
Kevin Wong <kevinwong@pursuit.org>
2019-10-19 15:59:27 -0400

    Problems 4 - 11 completed

--- array_methods_lab.js
+++ array_methods_lab.js
@@ -56,7 +56,15 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: [1,1,1,1,2]
 // Output: false
 // ```
-
+const sameElements = (arr) =>{
+    return arr.every((el) => {
+        if(el === arr[el]){
+            return true;
+        }
+    })
+}
+console.log(sameElements([1,1,1,1,1]));
+console.log(sameElements([1,1,1,1,2]));
 
 // 5. Write a function `countZeroes`, which takes an array of numbers as its argument and returns the amount of zeroes that occur in it.
 
@@ -64,6 +72,16 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: [1,0,0,2,0,3,0,0,0]
 // Output: 6
 // ```
+const countZeroes = (arr) => {
+    let count = 0;
+    arr.forEach( (el) => {
+        if (el === 0){
+            count += 1;
+        }
+    })
+    return count;
+}
+console.log(countZeroes([1,0,0,2,0,3,0,0,0]));
 
 // 6. Write a function that doubles every element in an array.
 
@@ -71,6 +89,12 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: [1,2,3,4]
 // Output: [2,4,6,8]
 // ```
+const double = arr => {
+    return arr.map((el) =>{
+        return el * 2
+    })
+}
+console.log(double([1, 2, 3, 4]));
 
 // 7. Write a function that whether every tripled number in an array is less than 20.
 
@@ -81,6 +105,14 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: [3,4,5]
 // Output: true
 // ```
+const tripleNum = (arr) => {
+    let newArr = arr.map((el) => {
+        return el * 3;
+    })
+    return newArr.every((el)=> el < 20)
+}
+console.log(tripleNum([1,2,4,7]));  //false
+console.log(tripleNum([3,4,5]));  //true
 
 // 8. Write a function `elementDivisibleBy` with parameters `divisor` and `arr`.  Return a new array of every element of arr that can be evenly divided by divisor.
 
@@ -88,13 +120,27 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: 5, [4,5,6,7,8,9,10,11,12,13,14,15]
 // Output: [5,10,15]
 // ```
+const elementDivisibleBy = (divisor, arr) => {
+    return arr.filter((el) => {
+        if(el % divisor === 0){
+            return el;
+        }
+    })
+}
+console.log(elementDivisibleBy(5, [4,5,6,7,8,9,10,11,12,13,14,15]));
 
-// 9. Write a `numberTimesIdx` that uses `map` and multiples each number in the array by its index.
+// 9. Write a function `numberTimesIdx` that uses `map` and multiples each number in the array by its index.
 
 // ```
 // Input: [6,7,8,9]
 // Output: [0,7,16,27]
 // ```
+const numberTimesIdx = (arr) => {
+    return arr.map((el, i) => {
+        return el * i;
+    })
+}
+console.log(numberTimesIdx([6,7,8,9]));
 
 // 10. Write a function that returns whether or not every value in an array is a positive even number that doesn't end in 0.
 
@@ -105,7 +151,15 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: [2,4,6,8,10]
 // Output: false
 // ```
-
+const posEvenNum = (arr) => {
+    return arr.every((el)=>{
+        if(el > 0 && el % 10 !== 0 ){
+            return el;
+        }
+    })
+}
+console.log(posEvenNum([2,4,6,8,12]));  // true
+console.log(posEvenNum([2,4,6,8,10]));  // false
 
 // 11. Write a function that returns a new array containing all of the strings in the original array uppercased.
 
@@ -113,26 +167,43 @@ console.log(evenNums([1,2,3,4,5]));
 // Input: [{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]
 // Output: ["CAT", "DOG", "BEAR"]
 // ```
+const upperCase = arr => {
+    let newArr = arr.filter((el)=>{
+        if(typeof el === 'string'){
+            return el;
+        }
+    })
+    return newArr.map((el)=>{
+        return el.toUpperCase();
+    })
+}
+console.log(upperCase([{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]));
 
 // ## Bonus:
 
 // You can write your own array methods!
 
 // ```js
-// Array.prototype.sayHi = function() {
-//   console.log("Hi")
-// }
+Array.prototype.sayHi = function() {
+    console.log(this)
+    console.log("Hi")
+}
 // ```
 
 // Now every array that you write has a `sayHi` method that you can call.
 
 // ```js
-// let myArr = [1,2,3]
-// myArr.sayHi() //Logs "Hi" to the console
+let myArr = [1,2,3]
+myArr.sayHi()   //Logs "Hi" to the console
 // ```
 
 
 // 1. Using Array.prototype add a new method `myForEach` that mimics the behavior of the built in `forEach`.
+Array.prototype.myForEach = function(cb){
+    for(let i = 0; i < this.length; i++){
+        cb(this[i]);
+    }
+}
 
 
 // 2. Use your `myForEach` to add `myMap` to the Array prototype. `myMap` should behave the same as regular `map`.
