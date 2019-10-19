a591fba0cd46e77e2c9cc3d017b97d6cf31598ea
Brandon Brown <brandonb0206@gmail.com>
2019-10-19 15:54:10 -0400

    Question 9 complete

--- arrayMethodsLab.js
+++ arrayMethodsLab.js
@@ -78,3 +78,13 @@ const elementDivisibleBy = (arr, divisor) => {
     })
 }
 console.log(elementDivisibleBy([4,5,6,7,8,9,10,11,12,13,14,15],5))
+
+//Question 9
+const numberTimesIdx = (arr) => {
+    return arr.map((el, i) => {
+        return el * i
+    })
+}
+console.log(numberTimesIdx([1, 10, 4, 5, 6]))
+
+//Question 10
