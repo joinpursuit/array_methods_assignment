af388f7b724af821c7712f766d752ed1f7bdd030
Kelvin <kelvinarellano@pursuit.org>
2019-10-19 14:34:41 -0400

    second done

--- array_methods.js
+++ array_methods.js
@@ -1,7 +1,18 @@
-//1
+//1.
 const arrByTen = (arr) => {
     return arr.map((el) => {
        return el + 10 
     })
 }
 console.log(arrByTen([1,3,5,7]))
+//2.
+const emptyArr = (arr) => {
+    return arr.map((el) => {
+        if(typeof(el) !== "string"){
+            return ""
+        }else{
+            return el
+        }
+    })
+}
+console.log(emptyArr(["a", 123, "b", "c", {name: "cat"}]))
