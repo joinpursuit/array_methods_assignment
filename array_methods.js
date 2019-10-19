c930c32a3cfb65d990ae115eb51b25d5f08ee743
Henrynunez112 <henrynunez@pursuit.org>
2019-10-19 14:56:01 -0400

    complete 1-2

---
+++ array_methods.js
@@ -0,0 +1,33 @@
+// 1. Given an array of numbers, write a function that returns a new array where every element is incremented by 10.
+
+// ```
+// Input: [1,2,3,4,5]
+// Output: [11,12,13,14,15]
+// ```
+
+
+// const byTen = (arr) => {
+//    return arr.map((el) => {
+//         return el += 10
+//     })
+// }
+// console.log(byTen([1,2,3,4,5]))
+
+// 2. Given an array, return a new array where every non-string element is converted to an empty string.
+
+// ```
+// Input: ["a", 123, "b", "c", {name: "cat"}]
+// // Output: ["a", "", "b", "c", ""]
+// ```
+
+const newStr = (arr) => {
+    return arr.map((el) => {
+        if(typeof el !== typeof String){
+            return el = ""
+        }else{
+            return el
+        }
+    })
+}
+
+console.log(newStr([1,2,3,"cat", "dog"]))
