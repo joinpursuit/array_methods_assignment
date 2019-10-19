32cede2812e7b2e0082c867c5b4018ac39ec3a9c
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-19 14:23:54 -0400

    complete problem 2

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ const incrementBy10 = (arr) => {
     })
 }
 
-console.log(incrementBy10([1,2,3,4,5])) ///[11,12,13,14,15]
+// console.log(incrementBy10([1,2,3,4,5])) ///[11,12,13,14,15]
+
+const onlyStrings = (arr) => {
+    return arr.map((el) => {
+        if(typeof el === 'string') {
+            return el
+        } else {
+            return ""
+        }
+    })
+}
+
+console.log(onlyStrings(["a", 123, "b", "c", {name: "cat"}])) //["a","","b","c",""]
