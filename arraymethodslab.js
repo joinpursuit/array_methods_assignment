4db51099117c9bfb88145151e0cc8f3e19ec5f67
ashya <ashyamanning@pursuit.org>
2019-10-19 16:03:21 -0400

    solving problem 11

--- arraymethodslab.js
+++ arraymethodslab.js
@@ -41,3 +41,72 @@ const checkAll = (arr) => {
 }
 console.log(checkAll([1,1,1,1,2]))
 console.log(checkAll([1,1,1,1,1]))
+
+
+// Problem 5
+const countZeroes = (arr) => {
+    let count = 0;
+    arr.forEach((el) => {
+        if (el === 0) {
+        count++;
+        }
+    })
+    return count;
+}
+console.log(countZeroes([1,0,0,2,0,0,3,0,0]))
+
+
+// Problem 6
+const doubles = (arr) => {
+    return arr.map((el) => {
+        return el *= 2;
+    })
+}
+console.log(doubles([1,2,3,4]))
+
+
+// Problem 7
+const everyTriple = (arr) => {
+    return arr.every((el) => {
+        return el * 3 <= 20
+    })
+}
+console.log(everyTriple([1,2,4]))
+
+
+// Problem 8
+const elementDivivbleBy = (arr, divisor) => {
+    return arr.filter((el) => {
+        return el % divisor === 0;
+    })
+}
+console.log(elementDivivbleBy([4,5,6,7,8,9,10,11,12,13,14,15], 3))
+
+
+// Problem 9
+const numberTimesIdx = (arr) => {
+    return arr.map((el, i) => {
+        return el * i;
+    })
+}
+console.log(numberTimesIdx([6,7,8,9]))
+
+
+// Problem 10
+const positiveEvenNums = (arr) => {
+    return arr.every((el) => {
+        return el % 2 === 0 && el % 10 !== 0 && el > 0;
+    })
+}
+console.log(positiveEvenNums([2,4,6,8,10]))
+
+
+// Problem 11
+const upperString = (arr) => {
+    return arr.filter((el) => {
+        if (typeof el === "string") {
+            return el
+        }
+    })
+}
+console.log(upperString([{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]))
