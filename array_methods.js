5a6b998453a4f485c0d0378ec9f66adbb77896fb
Kelvin <kelvinarellano@pursuit.org>
2019-10-19 17:00:32 -0400

    Complete

--- array_methods.js
+++ array_methods.js
@@ -32,12 +32,13 @@ const sameCheck = (arr) => {
 console.log(sameCheck([1,1,1,1]))
 //5.
 const countZeros = (arr) =>{
-    return arr.forEach((el) => {
-       let repeats = 0
+    let repeats = 0
+    arr.forEach((el) => {
         if (el === 0){
             repeats += 1
-        }return repeats
+        }
     })
+    return repeats
 }
 console.log(countZeros([1,0,0,2,0,3,0,0,0]))
 //6. 
@@ -46,3 +47,49 @@ const twoUp = arr => {
 }
 console.log(twoUp([1,2,3,4]))
 //7. 
+const trippleCheck = arr => {
+    return arr.every((el) => {
+        return el * 3 < 20
+    }
+)
+}
+console.log(trippleCheck([1,2,4,7]))
+//8.
+const elementDivisableBy = (divisor, arr) => {
+    return arr.filter((el) => {
+        if (el % divisor === 0){
+            return el
+        }
+    })
+}
+console.log(elementDivisableBy(5, [4,5,6,7,8,9,10,11,12,13,14,15]))
+//9.
+const arrByIndex = arr => {
+    return arr.map((el, i) => el * i)
+}
+console.log(arrByIndex([1,2,3,4]))
+//10.
+const  allEvensNoTen = arr => {
+    return arr.every((el) => {
+     return el % 2===0 && el % 10 !== 0
+    })
+}
+
+console.log(allEvensNoTen([1,2,3,1,4,10]))
+//11.
+// const upperArr = (arr) => {
+//     return arr.map((el) => {(
+//       el.filter("string") 
+//     )
+//     }    
+const upperCase = arr => {
+    let newArr = arr.filter((el)=>{
+        if(typeof el === 'string'){
+            return el;
+        }
+    })
+    return newArr.map((el)=>{
+        return el.toUpperCase();
+    })
+ }
+ console.log(upperCase([{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]))
