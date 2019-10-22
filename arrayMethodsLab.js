fe417b8923cf6636a413c48db18d7badb9f2b6da
Jay Fowler <jcafowler@gmail.com>
2019-10-21 20:49:41 -0400

    Complete Problem 5

--- arrayMethodsLab.js
+++ arrayMethodsLab.js
@@ -9,7 +9,7 @@ console.log(upTen([1,2,3]))
 const emptyString = (arr) => {
   return arr.map((el) => {
     if (typeof el !== typeof "a") {
-      el = ""
+      return ""
     }
     return el
   })
@@ -18,3 +18,33 @@ console.log(emptyString(["ape","shall","never",123,"kill",456,"ape"]))
 
 // 3. Write a function that returns only the even elements in an array of numbers.
 
+const evenOnly = (arr) => {
+  return arr.filter((el) => {
+    if (el % 2 === 0){
+      return el
+    }
+  })
+}
+console.log(evenOnly([1,2,3,4,5,6]))
+
+// 4. Write a function that checks if all elements in the array are the same.
+
+const dupeCheck = (arr) => {
+  return arr.every((el) => {
+    return arr[0] === el
+  })
+}
+console.log(dupeCheck([1,1,1,2]))
+
+// 5. Write a function `countZeroes`, which takes an array of numbers as its argument and returns the amount of zeroes that occur in it.
+
+const countZeroes = (arr) => {
+  zeroes = 0
+  arr.forEach((el) => {
+    if (el === 0){
+      zeroes++
+    }
+  })
+  return zeroes
+}
+console.log(countZeroes([0,3,4,0,1,14,5,0,0]))
