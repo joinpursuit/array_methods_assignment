8b78f9d9f26097fb921fb075a1f3bd781228855f
Brandon Brown <brandonb0206@gmail.com>
2019-10-26 17:49:44 -0400

    completed question thru 9

--- arrayMethods2.js
+++ arrayMethods2.js
@@ -47,3 +47,61 @@ const truthy = (arr) => {
     })
 }
 console.log(truthy([1,1,1,1,1,3,1,1]))
+
+//Question 5 
+
+const countZeroes = (arr) => {
+    return arr.reduce((acc = 0, el) => {
+        if(el === 0) {
+            acc++
+        }
+        return acc
+    }, 0)
+}
+console.log(countZeroes([0,0,0,0,0,1,2,3]))
+
+//Question 6
+
+const doubleAll = (arr) => {
+    return arr.reduce((acc, el) => {
+        acc.push(el * 2)
+        return acc
+    }, [])
+}
+console.log(doubleAll([1,2,3]))
+
+//Question 7
+
+const lessThan = (arr) => {
+    return arr.reduce((acc, el) => {
+        if(el * 3 > 20){
+            acc = false 
+        }else {
+            acc = true
+        }
+        return acc
+    })
+}
+console.log(lessThan([3,4,5,6]))
+
+//Question 8
+
+const elementDivisibleBy = (divisor, arr) => {
+    return arr.reduce((acc, el) => {
+        if(el % divisor === 0){
+            acc.push(el)
+        }
+        return acc
+    }, [])
+}
+console.log(elementDivisibleBy(2, [1,2,3,4,5,6,7,8]))
+
+//Question 9
+
+const numberTimesIdx = (arr) => {
+    return arr. reduce((acc, el, i) => {
+        acc.push(el * i)
+        return acc
+    }, [])
+}
+console.log(numberTimesIdx([1,2,3,4]))
