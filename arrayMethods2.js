15d827dfcde4d93c15cd28b987a1616e420aa8fa
Brandon Brown <brandonb0206@gmail.com>
2019-10-26 17:05:51 -0400

    completed question 3 and 4

--- arrayMethods2.js
+++ arrayMethods2.js
@@ -24,4 +24,26 @@ console.log(nonString(["blue", 5, "five", 2]))
 
 //QUESTION 3
 
-const 
+const evenArray = (arr) => {
+    return arr.reduce((newArr, el) => {
+        if(el % 2 === 0){
+            newArr.push(el)
+        }
+        return newArr
+    }, [])
+}
+console.log(evenArray([1,2,3,4,5,6,7,8]))
+
+//Question 4
+
+const truthy = (arr) => {
+    return arr.reduce((el, i) => {
+        if(el === arr[i]){
+            acc = true
+        }else {
+            acc = false
+        }
+        return acc
+    })
+}
+console.log(truthy([1,1,1,1,1,3,1,1]))
