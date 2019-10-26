8b896eae7053d4f42ff742b91a0af258ccfe44bc
Henrynunez112 <henrynunez@pursuit.org>
2019-10-26 17:44:26 -0400

    finished 1-8

--- array_methods_with_reduces.js
+++ array_methods_with_reduces.js
@@ -40,14 +40,45 @@
 // console.log(evenNum(([5,1,1,1,1,1,])))
 
 
-const allZeroes = (arr) => {
-    return arr.reduce ((acc, el) => {
-        if(el === 0){
-            acc++;
-        }else{
-            acc = 1
+// const allZeroes = (arr) => {
+//     return arr.reduce ((acc, el) => {
+//         if(el === 0){
+//             acc++;
+//         }else{
+//             acc = 1
+//         }
+//         return acc
+//     },0)
+// }
+// console.log(allZeroes([0,0,0,0,0,0,0,0]))
+
+// const allDoubles = (arr) => {
+//     return arr.reduce((acc, el) => {
+//         acc.push(el * 2)
+//         return acc
+//     },[])
+// }
+// console.log(allDoubles([1,2,3,4,5]))
+
+// const triNum = (arr) => {
+//     return arr.reduce((acc, el) => {
+//         if(el * 3 > 20){
+//             acc = false
+//         }else{
+//             acc = true
+//         }
+//         return acc
+
+//     },)
+// }
+// console.log(triNum([3,4,5]))
+
+const elementDivisibleBy = (divisor, arr) => {
+    return arr.reduce((acc, el) => {
+        if(el % divisor === 0){
+            acc.push(el)
         }
         return acc
-    },)
+    },[])
 }
-console.log(allZeroes([0,0,0,0,0,0,0,0]))
+console.log(elementDivisibleBy(4,[5,4,5,6,7,10,15,23,12,20]))
