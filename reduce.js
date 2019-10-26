a909801f84cd49fed7e56406b38fb67bea407772
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-10-26 17:14:39 -0400

    reducing problems 1-3

---
+++ reduce.js
@@ -0,0 +1,31 @@
+// 1. Given an array of numbers, write a function that returns 
+// a new array where every element is incremented by 10.
+
+// const increments = (arr) => {
+//     return arr.reduce((acc, el) => {
+//         acc.push(el + 10)
+//         return acc
+//     }, [])
+// }
+
+// console.log(increments([1, 2, 3, 4, 5]))
+
+// 2. Given an array, return a new array where every non-string element is 
+// converted to an empty string.
+
+
+const nonString = (arr) => {
+    return arr.reduce((acc,el) => {
+        if (typeof el === "string"){
+            acc.push(el)
+        } else {
+            el = "" 
+            acc.push(el)
+        }
+        return acc
+    },[])
+ }
+//console.log(nonString(['a','b',1]))
+console.log(nonString(["a", 123, "b", "c", {name: "cat"}]))
+
+// 3. 
