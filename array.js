2623d8e30916bc5eb9eb03cd59acec5cce4faea8
serami12 <stephanieramirez1@pursuit.org>
2019-11-21 16:39:37 -0500

    answered question four

--- array.js
+++ array.js
@@ -47,9 +47,29 @@ Output: [2, 4] */
 
 let inputThree = [1, 2, 3, 4, 5];
 
-const evenNumOnly = (arr) => {
+const getEvenNum = (arr) => {
     return arr.filter((el) => {
         return el % 2 !== 1
     })
 }
-console.log(evenNumOnly([1, 2, 3, 4, 5]))
+console.log(getEvenNum([1, 2, 3, 4, 5]))
+
+/* 
+4. Write a function that checks if all elements in the array are the same.
+Input: [1, 1, 1, 1, 1]
+Output: true
+
+Input: [1, 1, 1, 1, 2]
+Output: false */
+
+let inputFour = [1, 1, 1, 1, 1]
+
+const getSameElements = (arr) => {
+    let generalIndx = arr[0] //need to key into the  specific index to compare
+    return arr.every((el) => {
+        return el === generalIndx
+    })
+}
+// every returns a boolean 
+console.log(getSameElements([1, 1, 1, 1, 1]))
+console.log(getSameElements([1, 1, 1, 1, 2]))
