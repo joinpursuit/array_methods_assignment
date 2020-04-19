188252fc1b8e7b48839a2a2395b243972b728979
cassidybeni <cassidybeni@pursuit.org>
2020-04-18 20:09:28 -0400

    Complete problem 2

--- refactor.js
+++ refactor.js
@@ -6,3 +6,16 @@ const incrementByTen = (arr) => {
     })
 }
 console.log(incrementByTen([1,2,3,4,5]))
+
+//Question 2
+
+const convertNonstring = (arr) => {
+    return arr.map((el) => {
+        if (typeof(el) === typeof("")) {
+            return el
+        } else {
+            return ""
+        }
+    })
+}
+console.log(convertNonstring(["a", 123, "b", "c", { }]))
