ac7a5aadf00f5a0c00454b9f48596b911b9957ba
imran nazir <inazir2016@gmail.com>
2021-01-12 20:21:12 -0500

    Completed lab

---
+++ problems/test.js
@@ -0,0 +1,27 @@
+// /*As a group, elect a leader to share their screen.
+// Work as a group to complete the forEach function.
+// When your code is working, both test cases should behave as you expect.
+// */
+// // First Test Inputs
+// let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// const printDay = day => console.log(day)
+// // Second Test Inputs
+// let people = [
+//     {name: 'myra', age: 27},
+//     {name: 'heather', age: 55},
+//     {name: 'jennifer', age: 42}
+// ]
+// const printName = (person, index) => console.log(`The name of person ${index} is ${person.name}`)
+// // Complete this function!
+// function forEach(array, callback) {
+//     for (let i = 0; i < array.length; i += 1) {
+//         callback(array[i],i)
+//         // Complete the forEach function by invoking the callback.
+//         // Pass an element of arr as the first argument
+
+//         // Pass the index as the second argument
+//     }
+// }
+// // Now let's see if our function works!
+// forEach(days, printDay) // Prints Monday, then prints Tuesday, etc
+// forEach(people, printName) // Prints 'The name of person 0 is myra', then 'The name of person 1 is heather', etc
